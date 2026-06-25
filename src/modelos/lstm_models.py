dc2e00f63f62410b68fc11ab2799c861f3289504
Jennyfer2004 <125708775+Jennyfer2004@users.noreply.github.com>
2026-06-25 02:12:41 -0400

    Update lstm_models.py

--- src/modelos/lstm_models.py
+++ src/modelos/lstm_models.py
@@ -2,39 +2,33 @@ import tensorflow as tf
 from tensorflow.keras.layers import LSTM, Dense, Dropout, Bidirectional, Attention, Input
 from tensorflow.keras import Model
 
-def build_lstm_model(model_type, input_shape):
-    """
-    Construye y compila el modelo Deep Learning
-    Tipos: 'stacked', 'bidirectional', 'attention'
-    """
+def build_lstm_model(model_type, input_shape, lstm_u1=64, lstm_u2=32, dropout_rate=0.2, dense_u=32):
+    """Construye el modelo LSTM"""
     inputs = Input(shape=input_shape)
 
     if model_type == 'stacked':
-        x = LSTM(64, return_sequences=True)(inputs)
-        x = Dropout(0.2)(x)
-        x = LSTM(32)(x)
-        x = Dropout(0.2)(x)
+        x = LSTM(lstm_u1, return_sequences=True)(inputs)
+        x = Dropout(dropout_rate)(x)
+        x = LSTM(lstm_u2)(x)
+        x = Dropout(dropout_rate)(x)
 
     elif model_type == 'bidirectional':
-        x = Bidirectional(LSTM(64, return_sequences=True))(inputs)
-        x = Dropout(0.2)(x)
-        x = Bidirectional(LSTM(32))(inputs)
-        x = Dropout(0.2)(x)
+        x = Bidirectional(LSTM(lstm_u1, return_sequences=True))(inputs)
+        x = Dropout(dropout_rate)(x)
+        x = Bidirectional(LSTM(lstm_u2))(inputs)
+        x = Dropout(dropout_rate)(x)
 
     elif model_type == 'attention':
-        lstm_out = LSTM(64, return_sequences=True)(inputs)
-        query = Dense(64)(lstm_out)
-        value = Dense(64)(lstm_out)
+        lstm_out = LSTM(lstm_u1, return_sequences=True)(inputs)
+        query = Dense(lstm_u1)(lstm_out)
+        value = Dense(lstm_u1)(lstm_out)
         attention_layer = Attention()([query, value])
         
         x = tf.keras.layers.GlobalAveragePooling1D()(attention_layer)
-        x = Dense(32)(x)
+        x = Dense(dense_u)(x)
 
     outputs = Dense(1)(x)
-    
     model = Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
-    
     return model
 
 
